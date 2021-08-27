5f239821055d67c3cba680b9eb9912b1077a6d46
Grace Sutherland <rgs1690@gmail.com>
2021-08-27 15:38:55 -0500

    commented out bonus section in order to deploy MVP

--- src/javascripts/river/bearCreator.js
+++ src/javascripts/river/bearCreator.js
@@ -1,10 +1,10 @@
 import renderToDom from '../bearForm/renderToDom';
-import { fishCaught, attemptCatch } from './bearData';
+// import { fishCaught, attemptCatch } from './bearData';
 
-const filterAttempt = attemptCatch.filter((bear) => bear.attemptedFish);
-const filterAttemptTime = attemptCatch.filter((bear) => bear.attemptFishTime);
-const filterCaughtFish = fishCaught.filter((bear) => bear.fishCaught);
-const filterCaughtFishTime = fishCaught.filter((bear) => bear.attemptFishTime);
+// const filterAttempt = attemptCatch.filter((bear) => bear.attemptedFish);
+// const filterAttemptTime = attemptCatch.filter((bear) => bear.attemptFishTime);
+// const filterCaughtFish = fishCaught.filter((bear) => bear.fishCaught);
+// const filterCaughtFishTime = fishCaught.filter((bear) => bear.attemptFishTime);
 // console.warn(filterAttempt);
 // console.warn(filterAttemptTime);
 // console.warn(filterCaughtFish);
@@ -12,24 +12,24 @@ const filterCaughtFishTime = fishCaught.filter((bear) => bear.attemptFishTime);
 const bearCreator = (array) => {
   let domString = '';
   array.forEach((bear, i) => {
-  // // filter arrays for attempt and successful caught fish for current bear
-  // // do same loop for attempt
-    let attemptTimeDomString = '';
-    filterAttemptTime.forEach((attempt) => {
-      attemptTimeDomString += attempt.attemptFishTime;
-    });
-    let attemptDomString = '';
-    filterAttempt.forEach((attempt) => {
-      attemptDomString += attempt.attemptedFish;
-    });
-    let caughtDomString = '';
-    filterCaughtFish.forEach((attempt) => {
-      caughtDomString += attempt.fishCaught;
-    });
-    let caughtTimeDomString = '';
-    filterCaughtFishTime.forEach((attempt) => {
-      caughtTimeDomString += attempt.attemptFishTime;
-    });
+  // // // filter arrays for attempt and successful caught fish for current bear
+  // // // do same loop for attempt
+  //   let attemptTimeDomString = '';
+  //   filterAttemptTime.forEach((attempt) => {
+  //     attemptTimeDomString += attempt.attemptFishTime;
+  //   });
+  //   let attemptDomString = '';
+  //   filterAttempt.forEach((attempt) => {
+  //     attemptDomString += attempt.attemptedFish;
+  //   });
+  //   let caughtDomString = '';
+  //   filterCaughtFish.forEach((attempt) => {
+  //     caughtDomString += attempt.fishCaught;
+  //   });
+  //   let caughtTimeDomString = '';
+  //   filterCaughtFishTime.forEach((attempt) => {
+  //     caughtTimeDomString += attempt.attemptFishTime;
+  //   });
     // do foreach loop for html for caught fish
     // const caughtDomString = "";
     domString += `
@@ -37,14 +37,7 @@ const bearCreator = (array) => {
     <img src=${bear.url} class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${bear.name}</h5>
-         <button type="button" id='fishAttempt-${i}' class="btn btn-primary">Attempted to Catch Fish</button>
-       <button type="button" id='fishCaught-${i}' class="btn btn-success">Caught a Fish!</button>
-        <div>Attempt to catch Fish:</div>
-         <div >${attemptDomString}</div>
-         <div ${attemptTimeDomString}></div>
-         <div>Total Fish Caught:</div>
-         <div id='caughtDisplay'>${caughtDomString}</div>
-          <div id='totalDisplay'>${caughtTimeDomString}</div>
+    
        </div> 
   </div>
     `;
@@ -53,5 +46,12 @@ const bearCreator = (array) => {
 };
 //  interpolate caughtdomstring in the div on line 22 and 20
 //  interpolate attemptdomstring in div on line 18
-
+  //    <button type="button" id='fishAttempt-${i}' class="btn btn-primary">Attempted to Catch Fish</button>
+//  <button type="button" id='fishCaught-${i}' class="btn btn-success">Caught a Fish!</button>
+//   <div>Attempt to catch Fish:</div>
+//    <div >${attemptDomString}</div>
+//    <div ${attemptTimeDomString}></div>
+//    <div>Total Fish Caught:</div>
+//    <div id='caughtDisplay'>${caughtDomString}</div>
+//     <div id='totalDisplay'>${caughtTimeDomString}</div>
 export default bearCreator;
--- src/javascripts/river/buttonEvents.js
+++ src/javascripts/river/buttonEvents.js
@@ -1,33 +1,33 @@
-// import bearCreator from './bearCreator';
-import { bearArray, fishCaught, attemptCatch } from './bearData';
+// // import bearCreator from './bearCreator';
+// import { bearArray, fishCaught, attemptCatch } from './bearData';
 
-const attemptTimeTracker = (event) => {
-  event.preventDefault();
-  const targetId = event.target.id;
-  if (targetId.startsWith('fishAttempt')) {
-    console.warn(targetId);
-    const fishAttempt = bearArray.splice(targetId, 1);
-    attemptCatch.push(fishAttempt[0]);
-    attemptCatch[0].attemptFishTime += `${Date.now()}  `;
-    attemptCatch[0].attemptedFish += 1;
-    console.warn(attemptCatch);
-  }
-};
-const fishCatcher = (event) => {
-  const targetId = event.target.id;
-  if (targetId.startsWith('fishCaught')) {
-    console.warn(event.target.id);
-    const caughtFish = bearArray.splice(targetId, 1);
-    fishCaught.push(caughtFish[0]);
-    fishCaught[0].fishCaughtTime += Date.now();
-    fishCaught[0].fishCaught += 1;
-    console.warn(fishCaught);
-  }
-};
+// const attemptTimeTracker = (event) => {
+//   event.preventDefault();
+//   const targetId = event.target.id;
+//   if (targetId.startsWith('fishAttempt')) {
+//     console.warn(targetId);
+//     const fishAttempt = bearArray.splice(targetId, 1);
+//     attemptCatch.push(fishAttempt[0]);
+//     attemptCatch[0].attemptFishTime += `${Date.now()}  `;
+//     attemptCatch[0].attemptedFish += 1;
+//     console.warn(attemptCatch);
+//   }
+// };
+// const fishCatcher = (event) => {
+//   const targetId = event.target.id;
+//   if (targetId.startsWith('fishCaught')) {
+//     console.warn(event.target.id);
+//     const caughtFish = bearArray.splice(targetId, 1);
+//     fishCaught.push(caughtFish[0]);
+//     fishCaught[0].fishCaughtTime += Date.now();
+//     fishCaught[0].fishCaught += 1;
+//     console.warn(fishCaught);
+//   }
+// };
 
-const buttonEvents = () => {
-  document.querySelector('#river').addEventListener('click', attemptTimeTracker);
-  document.querySelector('#river').addEventListener('click', fishCatcher);
-};
+// const buttonEvents = () => {
+//   document.querySelector('#river').addEventListener('click', attemptTimeTracker);
+//   document.querySelector('#river').addEventListener('click', fishCatcher);
+// };
 
-export default buttonEvents;
+// export default buttonEvents;
